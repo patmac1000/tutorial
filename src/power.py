6fc3f74989b4f0d8b550f11fafc30fe1c31fbace
patmac1000 <pat.mccarthy@cit.ie>
2017-06-13 15:15:07 +0100

    Create power.py

--- src/power.py
+++ src/power.py
@@ -1,9 +1,11 @@
-def to_the power (x, y = 2) :
+def to_the power(x, y = 2) :
     result = x
     for i in range (0, y) :
-    result = result * x
+        result = result * x
     return result
 
-x = 10
-y = 3
-print ("10 to the power of 3 is: ", to_the_power (x, y))
+x = 9
+y = 6
+print("{0} to the power of {1} is {2}" .format(x, y, to_the_power(x, y)))
+
+
