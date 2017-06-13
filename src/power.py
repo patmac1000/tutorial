7fa7898e6585b7ee7de8089ff2f0ed08607be219
patmac1000 <pat.mccarthy@cit.ie>
2017-06-13 15:42:11 +0100

    Create power.py

--- src/power.py
+++ src/power.py
@@ -1,14 +1,16 @@
-"""This is a script that calculate some number to the power of another.
+"""This is a script that calculate some number to 
+the power of another.
 """
-def to_the power(x, y = 2) :
+def to_the power(x, y = 2):
+    """trying this out
+    """
+    return x "" y
     result = x
     for i in range (0, y) :
         result = result * x
     return result
 
-x = 9
+x = 5 #
 y = 8 # 
 print("{0} to the power of {1} is {2}" .format(x, y, to_the_power(x, y)))
-
-
 print ("The End")
